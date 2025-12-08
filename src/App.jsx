caf851c1135e5ebbf3b581520e38e8de9a7f6193
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-12-08 12:01:17 +0100

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react"
+
 
 function App() {
 
+  const [query, setQuery] = useState("");
+  const [hint, setHint] = useState([])
+
+  useEffect(() => {
+    if (!query.trim()) {
+      setHint([]);
+      return
+    }
+
+    fetch(`http://localhost:3333/products?search=${query}`)
+      .then(res => res.json())
+      .then(data => setHint(data))
+      .catch(err => console.error(err))
+  }, [query])
+
+  console.log(hint)
+
   return (
     <>
-
+      <div>
+        <h1>EX Autocomplete</h1>
+        <input
+          type="text"
+          value={query}
+          onChange={e => setQuery(e.target.value)}
+          placeholder="Cerca prodotto..."
+        />
+      </div>
+      {hint.length > 0 && (
+        <div className="dropdown">
+          {hint.map((p) => (
+            <p key={p.id}>{p.name}</p>
+          ))}
+        </div>
+      )}
     </>
   )
 }
